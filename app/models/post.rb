b7e327c8117c55caff89010de81edc0a4a8cc821
polygoning <polygoning@gmail.com>
2016-12-19 23:20:15 -0500

    Add relationship between posts and user models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  valdiates :user_id, presence: true
   has_attached_file :image, styles: { :medium => "640x" }
   validates :image, presence: true
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
