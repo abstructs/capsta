5bc00b0fb6a105e57a2aea11f5f769c2b608b316
polygoning <polygoning@gmail.com>
2016-12-28 14:53:29 -0500

    Override devise create action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def create
+    @user = User.new(sign_up_params)
+    if @user.save
+      sign_in(:user, @user)
+      redirect_to root_path
+    else
+      flash[:alert] = "Something went wrong, check the form!"
+      redirect_to new_user_registration_path
+    end
+  end
+
 private
-  
+
   def sign_up_params
     params.require(:user).permit(:email, :user_name, :password,
     :password_confirmation)
